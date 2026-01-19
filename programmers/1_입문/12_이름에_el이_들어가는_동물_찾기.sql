0d4778c382fcdd7ead1d49fb9aa297e39103ca6b
gorhkdwj <gorhkdwj@gmail.com>
2026-01-20 08:34:26 +0900

    [프로그래머스] 입문 - 이름에 el이 들어가는 동물 찾기 (#12)

--- programmers/1_입문/12_이름에_el이_들어가는_동물_찾기.sql
+++ programmers/1_입문/12_이름에_el이_들어가는_동물_찾기.sql
@@ -2,7 +2,7 @@
 -- 프로그래머스 입문 (⭐)
 -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59047
 -- 작성자: 김재천
--- 작성일: 2026. 01. 20. 08:33:51
+-- 작성일: 2026. 01. 20. 08:34:25
 
 select
     animal_id,
